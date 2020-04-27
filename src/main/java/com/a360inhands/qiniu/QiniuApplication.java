bb124a1cd02e7d31693f2f6d6cc34d0b320bc4b9
jishaofeng89 <1195957057@qq.com>
2020-04-27 21:47:03 +0800

    first commit

---
+++ src/main/java/com/a360inhands/qiniu/QiniuApplication.java
@@ -0,0 +1,13 @@
+package com.a360inhands.qiniu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QiniuApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QiniuApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/a360inhands/qiniu/QiniuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.a360inhands.qiniu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QiniuApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
