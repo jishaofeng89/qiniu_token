d066d0bb3c8548f873ab2a885672101db522b35f
jishaofeng89 <1195957057@qq.com>
2020-04-27 22:05:36 +0800

    first commit

---
+++ src/main/java/com/a360inhands/qiniu/controller/TokenController.java
@@ -0,0 +1,51 @@
+package com.a360inhands.qiniu.controller;
+
+import com.a360inhands.qiniu.utils.JsonBuilder;
+import com.a360inhands.qiniu.utils.TokenHelper;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@RestController
+@RequestMapping("/token")
+public class TokenController {
+
+    private JsonBuilder getJsonBuilder(int status, String info) {
+        return new JsonBuilder()
+                .put("status", status)
+                .put("info", info);
+    }
+
+    private JsonBuilder buildToken(String token) {
+        return getJsonBuilder(200, "success").put("token", token);
+    }
+
+    private JsonBuilder BAD(String info) {
+        return getJsonBuilder(400, info);
+    }
+
+    @RequestMapping("/get")
+    public void get(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+
+        resp.setHeader("Access-Control-Allow-Origin", "*");
+        resp.setHeader("Access-Control-Allow-Headers", "Authentication");
+        resp.setContentType("application/json;charset=utf-8");
+        String accessKey = req.getParameter("accessKey");
+        String secretKey = req.getParameter("secretKey");
+        String bucket = req.getParameter("bucket");
+        if (accessKey == null || accessKey.isEmpty() || secretKey == null || secretKey.isEmpty() || bucket == null || bucket.isEmpty()) {
+            resp.getWriter().write(BAD("参数不能为空，参数：accessKey,secretKey,bucket").getJson());
+            return;
+        }
+//        System.out.println(accessKey + "\n" + secretKey + "\n" + bucket);
+        TokenHelper tokenHelper = TokenHelper.create(accessKey, secretKey);
+        String token = tokenHelper.getToken(bucket);
+        String json = buildToken(token).getJson();
+//        System.out.println(json);
+        resp.getWriter().write(json);
+
+    }
+}
---
+++ src/main/java/com/a360inhands/qiniu/utils/TokenHelper.java
@@ -0,0 +1,74 @@
+package com.a360inhands.qiniu.utils;
+
+import javax.crypto.Mac;
+import javax.crypto.spec.SecretKeySpec;
+import java.io.UnsupportedEncodingException;
+import java.security.GeneralSecurityException;
+
+public class TokenHelper {
+
+    private String accessKey;
+    private SecretKeySpec secretKey;
+
+    private TokenHelper(String accessKey, SecretKeySpec secretKeySpec) {
+        this.accessKey = accessKey;
+        this.secretKey = secretKeySpec;
+    }
+
+    public static TokenHelper create(String accessKey, String secretKey) {
+        byte[] sk = utf8Bytes(secretKey);
+        SecretKeySpec secretKeySpec = new SecretKeySpec(sk, "HmacSHA1");
+        return new TokenHelper(accessKey, secretKeySpec);
+    }
+
+    public String getToken(String bucket) {
+        //token有效期为一个小时
+        long deadline = System.currentTimeMillis() / 1000L + get1Hour();
+        String s = jsonEncoder(bucket, String.valueOf(deadline));
+        return this.signWithData(utf8Bytes(s));
+    }
+
+    private String signWithData(byte[] data) {
+        String s = encodeToString(data);
+        return this.sign(utf8Bytes(s)) + ":" + s;
+    }
+
+    private String sign(byte[] data) {
+        Mac mac = this.createMac();
+        String encodedSign = encodeToString(mac.doFinal(data));
+        return this.accessKey + ":" + encodedSign;
+    }
+
+
+    private static String encodeToString(byte[] data) {
+        return Base64.encodeToString(data, 10);
+    }
+
+    private static byte[] utf8Bytes(String data) {
+        try {
+            return data.getBytes("UTF-8");
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+        return new byte[0];
+    }
+
+    private Mac createMac() {
+        try {
+            Mac mac = Mac.getInstance("HmacSHA1");
+            mac.init(this.secretKey);
+            return mac;
+        } catch (GeneralSecurityException var3) {
+            var3.printStackTrace();
+            throw new IllegalArgumentException(var3);
+        }
+    }
+
+    private Long get1Hour(){
+        return 3600L;
+    }
+
+    private String jsonEncoder(String bucket, String deadline) {
+        return "{\"scope\":\"" + bucket + "\",\"deadline\":" + deadline + "}";
+    }
+}
